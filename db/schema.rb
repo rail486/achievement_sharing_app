011c1266599b5529130d38f71acc765a05228157
rail486 <kenta.miyajima@nifty.com>
2021-06-19 08:41:46 +0900

    新規登録作成

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_082414) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
