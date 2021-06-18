1ae137ab38b60b1c1b1fd7d619321947a2fc0231
rail486 <kenta.miyajima@nifty.com>
2021-06-18 17:38:50 +0900

    データ仕様変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 120 }
   validates :achievement, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
--- db/migrate/20210618082414_create_tasks.rb
+++ db/migrate/20210618082414_create_tasks.rb
@@ -1,11 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :user_id
       t.date :date
       t.string :content
       t.integer :achievement
       t.boolean :share
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
