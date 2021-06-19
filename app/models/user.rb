0b2e3e0d110011931330cc3419844dc26720c489
rail486 <kenta.miyajima@nifty.com>
2021-06-20 01:39:40 +0900

    エラーメッセージ日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
-  validates :uid, presence: true, uniqueness: { case_sensitive: false}, length: { maximum: 30 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :uid, presence: true, uniqueness: { case_sensitive: false}, length: { maximum: 30 }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: :invalid_uid }
   has_secure_password
-  validates :password, presence: true, length: { in: 10..30 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password, presence: true, length: { in: 10..30 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: :invalid_password }
 end
