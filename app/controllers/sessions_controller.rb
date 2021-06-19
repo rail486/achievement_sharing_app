2c86a9a424a584b9bf92db6553d94701dacde38b
rail486 <kenta.miyajima@nifty.com>
2021-06-20 06:41:16 +0900

    flash追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'IDまたはパスワードが正しくありません'
       render 'new'
     end
   end
