ffb43f54820cb8f21f80afb95132aab1a3d642fb
rail486 <kenta.miyajima@nifty.com>
2021-06-21 05:12:21 +0900

    アカウント設定追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   get 'timelines/index'
   root    'top_pages#home'
-  get     '/signup'   => 'users#new'
-  get     '/settings' => 'users#setting'
-  get     '/login'    => 'sessions#new'
-  post    '/login'    => 'sessions#create'
-  delete  '/logout'   => 'sessions#destroy'
-  resources :users
+  get     '/signup'                  => 'users#new'
+  get     '/settings'                => 'users#setting'
+  get     '/users/:id/edit_profile'  => 'users#edit_profile'
+  get     '/users/:id/edit_password' => 'users#edit_password'
+  get     '/login'                   => 'sessions#new'
+  post    '/login'                   => 'sessions#create'
+  delete  '/logout'                  => 'sessions#destroy'
+  resources :users do
+    member do
+      patch :update_profile
+      patch :update_password
+    end
+  end
 end
