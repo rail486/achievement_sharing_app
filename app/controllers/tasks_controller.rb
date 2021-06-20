b038bfa3e48d1721d0e756c1fdcab163709ee4b7
yuto7116 <nishiya0715@gmail.com>
2021-06-20 17:25:26 +0900

    一覧＆編集の画面

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+  #
+  def index
+    @unshared_tasks = current_user.tasks.where(date: session[:date]).where(share: false)
+    @shared_tasks = current_user.tasks.where(date: session[:date]).where(share: true)
+    #@unshared_tasks = Task.where(userid: current_user.userid).where(date: session[:date]).where(share: false)
+    #@shared_tasks = Task.where(userid: current_user.userid).where(date: session[:date]).where(share: true)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(session[:date]), primary: "タスク#{@task.content}を削除しました"
+  end
+
+  def finish
+    @task = Task.find(params[:id])
+    @task.update_attribute(:achievement, 100)
+    redirect_to tasks_path(session[:date]), primary: "タスク#{@task.content}を完了しました"
+  end
+
+  def share
+    @task = Task.find(params[:id])
+    @task.update_attribute(:share, true)
+    redirect_to tasks_path(session[:date]), primary: "タスク#{@task.content}を共有しました"
+  end
+
+  def unshare
+    @task = Task.find(params[:id])
+    @task.update_attribute(:share, false)
+    redirect_to tasks_path(session[:date]), primary: "タスク#{@task.content}の共有をやめました"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :achievement)
+  end
+
+end
--- db/migrate/20210618082414_create_tasks.rb
+++ db/migrate/20210618082414_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
     create_table :tasks do |t|
       t.date :date
       t.string :content
-      t.integer :achievement
+      t.integer :achievement, default:0
       t.boolean :share
       t.references :user, null: false, foreign_key: true
 
