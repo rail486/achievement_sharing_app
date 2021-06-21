5dae629564fdb00a40a557ff75dcf4b0a4203e6f
rail486 <kenta.miyajima@nifty.com>
2021-06-22 05:31:27 +0900

    フォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   has_many :tasks, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :uid, presence: true, uniqueness: { case_sensitive: false}, length: { maximum: 30 }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: :invalid_uid }
   validates :introduction, length: { maximum: 120 }
@@ -43,5 +48,17 @@ class User < ApplicationRecord
     else
       User.all
     end
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
