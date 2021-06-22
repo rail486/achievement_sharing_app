0a209ceb3378449ecab33e8cea15f0c289ed507b
rail486 <kenta.miyajima@nifty.com>
2021-06-22 16:39:37 +0900

    feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,4 +61,9 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
 end
