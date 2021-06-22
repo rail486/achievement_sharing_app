bb19e0efb90935639fb4a459c31d0dd000573bda
alkk121 <al19121@shibaura-it.ac.jp>
2021-06-22 17:27:27 +0900

    フォロワーのみ閲覧(タイムライン)

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,6 @@ class User < ApplicationRecord
 
   def feed
     following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+    Task.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
 end
