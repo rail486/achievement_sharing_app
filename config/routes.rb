dc5e10ed1616f51debc7088c39f4a38e20532909
alkk121 <al19121@shibaura-it.ac.jp>
2021-06-22 14:37:13 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   
   #timeline
   get     'tasklist'                  => 'tasklists#index'
-  #get     'calendar'                  => 'calendars#index'
+  get     'calendar'                  => 'calendars#index'
 
   resources :users do
     member do
