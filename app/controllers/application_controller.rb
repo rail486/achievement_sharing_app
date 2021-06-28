556a8e0d9c54ce56efcf1d74dcbafe239e9a9356
rail486 <kenta.miyajima@nifty.com>
2021-06-29 08:08:39 +0900

    微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,4 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :danger
 
-  private
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください"
-        redirect_to "/login"
-      end
-    end
 end
