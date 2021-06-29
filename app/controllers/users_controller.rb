08f476ee241c44092a92d499fae3e5a30c954f29
alkk121 <al19121@shibaura-it.ac.jp>
2021-06-29 14:53:49 +0900

    プロフィール、タイムラインの構成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tasks = current_user.feed.where(share: true).paginate(page: params[:page])
   end
 
   def new
